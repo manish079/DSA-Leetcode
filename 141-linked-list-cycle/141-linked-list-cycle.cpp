b32eeefe267fd493254ce903fb857c92f346a4ec
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-08-23 10:03:19 +0530

    Time: 24 ms (22.28%), Space: 11.3 MB (7.72%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -12,20 +12,38 @@ public:
         
         //Approach-1 fast and slow ptr
         
-        if(head==NULL){
-            return false;
-        }
+//         if(head==NULL){
+//             return false;
+//         }
         
-        ListNode *fast=head;
-        ListNode *slow=head;
+//         ListNode *fast=head;
+//         ListNode *slow=head;
         
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+//         while(fast!=NULL && fast->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
             
-            if(fast==slow)
+//             if(fast==slow)
+//                 return true;
+//         }
+//         return false;
+        
+        
+        //Approach-2 HashMap
+        
+        unordered_map<ListNode*, bool> visited;
+        ListNode *temp=head;
+        
+        while(temp!=NULL){
+            
+            //cycle is present
+            if(visited[temp] == true)
                 return true;
+            //mark node visited if cycle not detect
+            visited[temp] = true;
+            temp=temp->next;
         }
         return false;
+        
     }
 };
