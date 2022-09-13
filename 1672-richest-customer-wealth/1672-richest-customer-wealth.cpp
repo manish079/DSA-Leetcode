12d29a303c49c6e4699a7928e96187928b3c0aa3
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-09-13 16:58:59 +0530

    Time: 3 ms (96.83%), Space: 7.8 MB (87.56%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -5,15 +5,20 @@ public:
         int total_banks = accounts[0].size();
         
         int highest_money = 0;
-        vector<int> v;
+        // vector<int> v;
+        
+        int curr_mon=0;
         
         for(int i=0; i<total_customer; i++){
             highest_money = 0;
             for(int j=0; j<total_banks; j++){
                 highest_money = highest_money + accounts[i][j];
             }
-            v.push_back(highest_money);
+            // v.push_back(highest_money);
+            if(curr_mon < highest_money)
+                curr_mon = highest_money;
         }
-        return *max_element(v.begin(),v.end());
+        // return *max_element(v.begin(),v.end());
+        return curr_mon;
     }
 };
