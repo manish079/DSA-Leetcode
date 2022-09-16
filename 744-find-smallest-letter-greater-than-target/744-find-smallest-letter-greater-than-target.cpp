7ba5d6c14aa126005f9f04a68b3f450e1c8539dd
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-09-16 08:07:02 +0530

    Time: 18 ms (83.73%), Space: 15.9 MB (70.42%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -5,6 +5,9 @@ public:
         int s = 0;
         int e = letters.size()-1;
         
+        if(target >= letters[e])
+            return letters[0];
+        
         while(s<=e){
             int mid = s + (e-s)/2;
            if(letters[mid] > target)
@@ -12,7 +15,8 @@ public:
             else
                 s = mid+1;
         }
-        return letters[s%letters.size()];
+        // return letters[s%letters.size()];
+        return letters[s];
         
     }
 };
