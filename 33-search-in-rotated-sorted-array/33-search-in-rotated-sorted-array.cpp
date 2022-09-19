8ba25f9901e10beb8d852bff53ca276e7d8665b1
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-09-19 15:35:53 +0530

    Time: 4 ms (74.59%), Space: 11.1 MB (29.10%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
+    
+   
     int search(vector<int>& nums, int target) {
         
-        //Brute force
-
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target){
-                return i;
-        }
-        }
-        return -1;    
+        //Brute force o(n)
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        // }
+        // return -1;    
         
         //Optimize using Binary Search
-     
+       
         
+        int s=0;
+        int e = nums.size()-1;
+        
+        
+         while(s<=e){
+            int mid = (s+e)>>1;
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            //array left part 0 to pivot
+            if(nums[s] <= nums[mid]){
+                 if(target >= nums[s] && target <= nums[mid])
+                     e = mid-1;
+                else
+                    s = mid+1;
+            }
+            else{
+                //right part of pivot pivot to end-1
+                if(target >= nums[mid] && target <= nums[e])
+                    s = mid+1;
+                else
+                    e = mid-1;
+            }
+        }
+        return -1;
         
     }
 };
