8d1035cf4fd98667840a20c9a31c4658cde66fe5
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-09-22 17:03:29 +0530

    Time: 1 ms (87.68%), Space: 44 MB (35.43%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,38 +1,76 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        
-        int s=0;
-        int e = nums.length-1;
-        
-        
-         while(s<=e){
-            int mid = (s+e)>>1;
-            
-            if(nums[mid]==target)
+
+        int pivot = findPivot(nums);
+      
+        // if -1 return then array is normal array in sorted and just apply normal
+        // binary search
+        if (pivot == -1) {
+            return binary_search(nums, 0, nums.length - 1, target);
+        }
+
+        // If pivot found we get two ascendng array
+        if (nums[pivot] == target)
+            return true;
+        if (target >= nums[0] && target <= nums[pivot])
+            return binary_search(nums, 0, pivot - 1, target);
+        else
+            return binary_search(nums, pivot + 1, nums.length - 1, target);
+
+    }
+
+    public static int findPivot(int[] nums) {
+
+        int s = 0;
+        int e = nums.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            // four cases for peak
+
+            if (mid < e && nums[mid] > nums[mid + 1]) // handle array cannot exceed its length
+                return mid;
+            if (mid > s && nums[mid] < nums[mid - 1]) // mid previous value is greater than mid
+                return mid - 1;
+
+            // Here only this conditions are checked if duplicate found
+            // It might be case of mid==start and mid==end then Just skips duplicate using
+            // s++ and e--
+            // before skipping firstly we check if thats skipped value can be a pivot so
+            // check
+
+            if (nums[mid] == nums[s] && nums[mid] == nums[e]) {
+                if (mid > s  && nums[s] > nums[s + 1])
+                    return s;
+                s++;
+
+                if (mid < e && nums[e] < nums[e - 1])
+                    return e - 1;
+                e--;
+            }
+            // left side is sorted, So pivot should be in right
+            else if ((nums[s] <= nums[mid]) || (nums[s] == nums[mid] && nums[e] <= nums[mid]))              {
+                s = mid + 1;
+            } else
+                e = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static boolean binary_search(int[] nums, int s, int e, int target) {
+
+        while (s <= e) {
+            int mid = (s + e) >> 1;
+
+            if (nums[mid] == target)
                 return true;
-            
-            //array left part 0 to pivot
-            if(nums[s] <= nums[mid]){
-                 if(target >= nums[s] && target <= nums[mid])
-                     e = mid-1;
-                else if(nums[mid]==nums[s]){
-                    s = s+1;
-                }
-                else
-                    s = mid+1;
-            }
-            else{
-                //right part of pivot pivot to end-1
-                if(target >= nums[mid] && target <= nums[e])
-                    s = mid+1;
-                else if(nums[mid] == nums[e])
-                    e--;
-                else
-                    e = mid-1;
-            }
+            else if (nums[mid] > target)
+                e = mid - 1;
+            else
+                s = mid + 1;
         }
         return false;
-        
     }
-    
 }
