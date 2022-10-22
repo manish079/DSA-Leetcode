75de5c88dd9204b6731d9436f6ab9d88fcbf8bb2
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-10-22 18:03:33 +0530

    Time: 2 ms (40.84%), Space: 51.6 MB (20.45%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,19 @@
 class Solution {
-    public int missingNumber(int[] arr) {
+    public int missingNumber(int[] arr) { 
         
-        int natural = arr.length*(arr.length+1)/2;
-        int sum=0;
-        for(int i=0; i<arr.length; i++)
-            sum += arr[i];
-        int res = natural-sum;
-        return res;
+     int H[] = new int[arr.length+1];
+        Arrays.fill(H,0);
+        
+      
+    for(int i=0; i<arr.length; i++){
+        H[arr[i]]++;
     }
+    
+        for(int i=1; i<H.length; i++){
+            if(H[i]==0){
+                return i;
+            }
+        }
+        return 0;
+  }
 }
