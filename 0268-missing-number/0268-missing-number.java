bc8c63a9d335fb470381632e6c131794523a3523
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-10-22 18:06:12 +0530

    Time: 2 ms (40.84%), Space: 52.3 MB (9.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -4,10 +4,9 @@ class Solution {
      int H[] = new int[arr.length+1];
         Arrays.fill(H,0);
         
-      
-    for(int i=0; i<arr.length; i++){
-        H[arr[i]]++;
-    }
+        for(int i=0; i<arr.length; i++){
+            H[arr[i]]++;
+        }
     
         for(int i=1; i<H.length; i++){
             if(H[i]==0){
