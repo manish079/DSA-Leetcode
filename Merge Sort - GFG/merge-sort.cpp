526805f095c860376e5467034b43b8b3839800ff
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-11-01 19:39:44 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.cpp
+++ Merge Sort - GFG/merge-sort.cpp
@@ -60,8 +60,9 @@ class Solution
     public:
     void mergeSort(int arr[], int l, int r)
     {
-        int mid = (l+r)/2;
+       
         if(l<r){
+             int mid = (l+r)/2;
             mergeSort(arr, l, mid);
             mergeSort(arr, mid+1, r);
             merge(arr, l, mid, r);
