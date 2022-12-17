c1ef6706f2345345001304b0a13196b90b623c5d
Manish Kumar <84091739+manish079@users.noreply.github.com>
2022-12-17 23:49:58 +0530

    Time: 21 ms (60.06%), Space: 12.2 MB (52.63%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,48 +1,37 @@
-class Solution
-{
-    public:
-
-        int solve(vector<string> &str)
-        {
-
-            stack<int> s;
-
-            for (auto it: str)
-            {
-               	// if operator found pop element and apply operator
-                if (it == "+" || it == "-" || it == "*" || it == "/")
-                {
-                    int b = s.top();
-                    s.pop();
-                    int a = s.top();
-                    s.pop();
-
-                    int res;
-
-                    if (it == "+")
-                        res = a + b;
-                    if (it == "-")
-                        res = a - b;
-                    if (it == "*")
-                        res = long(a) * long(b);
-                    if (it == "/")
-                    {
-                        res = a / b;
-                    }
-                    s.push(res);
-                }
-                else
-                {
-                    s.push(stoi(it));
-                }
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+     
+        
+        //using unordered_map lambda
+        
+        unordered_map<string, function <int(int a, int b)>> m ={
+            {"+", [](int a,int b) {return a+b;}},
+            {"-", [](int a,int b){return a-b;}},
+            {"*", [](int a,int b){return long(a)*long(b);}},
+            {"/", [](int a,int b){ return a/b;}},
+        };
+        
+        
+        stack<int> s;
+        
+        
+        for(string &token:tokens){
+            if(token=="+" || token=="-" || token=="*" || token=="/"){
+                int b = s.top();
+                s.pop();
+                int a = s.top();
+                s.pop();
+                
+                
+                
+                int ans = m[token](a,b);
+                
+                s.push(ans);
             }
-            int ans = s.top();
-            s.pop();
-            return ans;
+            else
+                s.push(stoi(token));
         }
-    int evalRPN(vector<string> &tokens)
-    {
-
-        return solve(tokens);
+        return s.top();
     }
 };
