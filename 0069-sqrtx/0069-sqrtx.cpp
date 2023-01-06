6576e169a41f430784c094b5848b13cafe87126f
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-01-06 18:19:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        
+       long long int s = 1, e = x;
+        
+        while(s<=e){
+           long long int mid = (s+e)>>1;
+            
+            // perfect squre case
+            if(mid*mid == x)
+                return mid;
+            
+            if((mid*mid) > x)
+                e = mid-1;
+            else
+                s = mid+1;
+        }
+        return s-1;
     }
 };
