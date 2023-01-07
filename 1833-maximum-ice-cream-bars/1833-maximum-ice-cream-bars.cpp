b2188ee4f90cfa110e36894975813c24048c62da
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-01-07 16:19:26 +0530

    Time: 168 ms (98.82%), Space: 105 MB (5.41%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,21 +1,47 @@
-class Solution {
-public:
-    int maxIceCream(vector<int>& costs, int coins) {
-        
-        int ans  = 0;
-        
-        sort(costs.begin(), costs.end());
-        
-        if(coins < costs[0])
-            return 0;
-        
-        for(int i=0; i<costs.size(); i++){
-          
-            if(coins >= costs[i]){
-                ans++;
-                coins-=costs[i];
+class Solution
+{
+    public:
+        int maxIceCream(vector<int> &costs, int coins){
+            
+            //without using sorting
+            
+            vector<int> hash(100001, 0);
+            int ans=0;
+            
+            //fill elements inside hash 
+            for(int i=0; i<costs.size(); i++){
+                hash[costs[i]]++;
             }
-        }
-        return  ans;
+            
+            int i=0;
+            while(i<100001 && coins >= i){
+                
+                
+                  
+                //If coins remaining but can't buy all icecream just I check can I buy 1 or2 or3..
+                if((long)hash[i]*i > coins && coins>=i){
+                    
+                    int less = coins/i;
+                    coins = coins-less*i;
+                    ans+=less;
+                    break;
+                }
+                
+              
+                // hash[i]*i <= coins
+                if(hash[i]!=0 && coins>=0){
+                    
+                    coins = coins-hash[i]*i;
+                    ans+=hash[i];
+                }
+              
+                
+                i++;
+            }
+            
+            return ans;
+            
     }
+
+            
 };
