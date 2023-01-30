331fa5613aef64ce83871317c62d251d0c889f7d
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-01-30 22:15:14 +0530

    Time: 3 ms (38.83%), Space: 6 MB (73.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+        
+        //How I got idea:-
+        
+        // to understand question and example and I find a pattern
+        // 
+        
+        // t3 = 0+1+1 = 2;
+        // t4 = 1+1+2=4
+        // t5 = 1+2+4=7
+        // t6 = 2+4+7=13
+        // upto size of n is 38
+        
+        
+        //space complexity will be O(38) or still O(1).
+        //Time complexity will be O(1)
+        int arr[38] = {0};
+        
+        arr[0] = 0, arr[1] = 1, arr[2]=1;
+        
+        for(int i=3; i<38; i++){
+            arr[i] = arr[i-1]+arr[i-2]+arr[i-3];
+        }
+        
+        return arr[n];
+    }
+};
