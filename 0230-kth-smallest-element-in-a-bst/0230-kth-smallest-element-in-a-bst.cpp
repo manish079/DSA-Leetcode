76dd8abeaeeb76e55ad91f7341ef5b4a57995031
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-02-07 22:51:30 +0530

    Time: 19 ms (72.34%), Space: 24.4 MB (22.06%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -8,7 +8,8 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ **/
+
 class Solution {
 public:
     void inorder(TreeNode* root, vector<int> &vec){
