c2e358ca280118e13df246052508376805677af3
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-02-12 23:07:35 +0530

    Time: 47 ms (40.60%), Space: 28.2 MB (58.98%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -7,7 +7,7 @@ class Solution
            	//using two pointer approach in O(n)
             int mini = INT_MAX;
             int sum = 0;
-            bool flag = false;
+            // bool flag = false;
 
             int i = 0, j = 0;
 
@@ -15,15 +15,17 @@ class Solution
             {
                 sum += nums[j++];
 
+                //reducing size of window
                 while (sum >= target)
                 {
                     mini = min(mini, j - i);
                    	//search more possible answer
                     sum -= nums[i++];
-                    flag = true;
+                    // flag = true;
                 }
             }
 
-            return flag ? mini : 0;
+            // return flag ? mini : 0;
+              return mini==INT_MAX? 0 : mini;
         }
 };
