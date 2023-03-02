15eebd5fcaa146c22b4bc0eb84bd267db547c93e
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-03-02 17:02:20 +0530

    Time: 26 ms (35.43%), Space: 19.5 MB (79.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,47 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int count = 1;
-        
-        sort(nums.begin(), nums.end());
-             
-       for(int i=0; i<nums.size()-1; i++){
-           if(nums[i]==nums[i+1]){
-               count++;
-               
-               if(count > nums.size()/2)
-                   return nums[i];
-           }
-           else{
-               count = 1;
-           }
-       }
-             return 1;
-    }
+class Solution
+{
+    public:
+        int majorityElement(vector<int> &nums)
+        {
+           	//         int count = 1;
+
+           	//         sort(nums.begin(), nums.end());
+
+           	//        for(int i=0; i < nums.size()-1; i++){
+           	//            if(nums[i]==nums[i+1]){
+           	//                count++;
+
+           	//                if(count > nums.size()/2)
+           	//                    return nums[i];
+           	//            }
+           	//            else{
+           	//                count = 1;
+           	//            }
+           	//        }
+           	//              return 1;
+
+            
+            //boys moore algo, O(N)
+            int vote = 1;
+            int majEle = nums[0];
+
+            for (int i = 0; i < nums.size(); i++)
+            {
+                int res = nums[i];
+                if (nums[i] == majEle)
+                {
+                    vote++;  //current party leading
+                }
+                else
+                {
+                    vote--;  //current part loosing
+                }
+                
+                if(vote == 0){    //new party in lead
+                    vote = 1;
+                    majEle = res; 
+                }
+            }
+            return majEle;
+        }
 };
