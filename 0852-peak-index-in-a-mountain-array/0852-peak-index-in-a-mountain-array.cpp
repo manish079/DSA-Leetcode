2a31fa38a55f55de5527782fe29202799433b4df
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-03-07 06:55:32 +0530

    Time: 123 ms (55.21%), Space: 59.7 MB (26.29%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -8,19 +8,31 @@ class Solution
 
             int s = 0, e = n - 1;
 
-            while (s <= e)
-            {
-                int mid = (s + e) / 2;
+           	//             while (s <= e)
+           	//             {
+           	//                 int mid = (s + e) / 2;
 
-                if ( arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
-                    return mid;
-                else if (arr[mid] < arr[mid + 1])
+           	//                 if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
+           	//                     return mid;
+           	//                 else if (arr[mid] < arr[mid +1])
+           	//                 {
+           	//                     s = mid + 1;
+           	//                 }
+           	//                 else
+           	//                     e = mid - 1;
+           	//             }
+           	//             return -1;
+
+            while (s < e)
+            {
+                int mid = s + (e - s) / 2;
+                if (arr[mid] > arr[mid + 1])
                 {
-                    s = mid + 1;
+                    e = mid;
                 }
                 else
-                    e = mid - 1;
+                    s = mid + 1;
             }
-            return -1;
+            return e;
         }
 };
