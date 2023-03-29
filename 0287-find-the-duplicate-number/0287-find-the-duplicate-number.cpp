a44b8e526883197e559a4a263456a4ea0211a32e
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-03-29 21:40:52 +0530

    Time: 174 ms (24.81%), Space: 61.3 MB (67.32%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,12 +1,27 @@
-class Solution
-{
-    public:
-        int findDuplicate(vector<int> &nums) {
-          
-            while(nums[0] != nums[nums[0]]){
-                swap(nums[0], nums[nums[0]]);
-            }
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        
+        // using XOR
+        
+        int s=1, e=nums.size()-1;  // bcoz arr is [1,n]
+        
+        while(s<=e){
+            int mid = (s+e)/2;
             
-            return nums[0];
+            int count = 0;
+            
+            for(int i=0; i<nums.size(); i++){
+                if(nums[i] <= mid){
+                    count++;
+                }
+            }
+            if(count <= mid)
+                s = mid+1;
+            else
+                e = mid-1;
         }
+        return s;
+    }
 };
