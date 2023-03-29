803d01e47224a4d984b2c18b879ce2614c44a408
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-03-29 17:05:20 +0530

    Time: 107 ms (87.05%), Space: 61.3 MB (30.79%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,23 +2,11 @@ class Solution
 {
     public:
         int findDuplicate(vector<int> &nums) {
-            
-            int ans = -1;
-            
-            for(int i=0; i<nums.size(); i++){
-                
-                int ind = abs(nums[i]);
-                
-                //If found duplicate element
-                if(nums[ind] < 0){
-                    ans = ind;
-                    break;
-                }
-                else{
-                    //visited mark
-                    nums[ind] *= -1; 
-                }
+          
+            while(nums[0] != nums[nums[0]]){
+                swap(nums[0], nums[nums[0]]);
             }
-            return ans;
+            
+            return nums[0];
         }
 };
