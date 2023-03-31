2f2a8910afebd85c9e3d5182d218cc9cbf904271
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-03-31 15:27:04 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -10,7 +10,7 @@ class Solution
        vector <int> commonElements (int arr1[], int arr2[], int arr3[], int n1, int n2, int n3)
         {
             vector<int> result;
-            unordered_set<int>s; 
+          
 
             int i = 0, j = 0, k = 0;
 
