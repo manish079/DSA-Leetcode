126409ace92427e276378565bec7e40fe1e39d96
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-04-10 21:32:58 +0530

    Time: 8 ms (42.20%), Space: 10.8 MB (11.63%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -4,24 +4,21 @@ public:
         
         vector<int> ans;
         
-       sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-        int i = 0, j = 0;
+        unordered_map<int, int> m;
         
-        while(i < nums1.size() && j < nums2.size()){
-            if(nums1[i] == nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i] < nums2[j]){
-                i++;
-            }
-            else{
-                j++;
+        for(int i = 0; i<nums1.size(); i++)
+            m[nums1[i]]++;
+        
+        
+        for(int i=0; i<nums2.size(); i++){
+            
+            if(m[nums2[i]] > 0){
+                ans.push_back(nums2[i]);
+                
+                m[nums2[i]]--;
             }
         }
-       
+        
         return ans;
     }
 };
