6705c7d3e58a00efed7ed85e605b7dc022380870
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-04-14 16:32:49 +0530

    Time: 141 ms (27.64%), Space: 15 MB (40.37%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,19 +1,24 @@
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-         int row = 0, col = matrix[0].size()-1;
-        
-        while(row < matrix.size() && col >=0){
-            if(matrix[row][col]==target)
-                return true;
-            else if(target > matrix[row][col]  ){
-                row++;
+class Solution
+{
+    public:
+        bool searchMatrix(vector<vector < int>> &matrix, int target)
+        {
+
+            if (target < matrix[0][0] && matrix.size() == 0)
+                return false;
+
+            int row = 0, col = matrix[0].size() - 1;
+
+            while (row < matrix.size() && col >= 0)
+            {
+
+                if (matrix[row][col] == target)
+                    return true;
+                else if (matrix[row][col] < target)
+                    row++;
+                else
+                    col--;
             }
-            else{
-                col--;
-            }
+            return false;
         }
-        return false;
-    }
 };
