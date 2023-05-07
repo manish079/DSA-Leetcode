ab4273276a340fe678f3e0d2491d3919fa946905
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-05-07 20:11:46 +0530

    Time: 4 ms (34.49%), Space: 6.8 MB (73.46%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,47 +1,39 @@
-class MyStack
-{
-    public:
-        queue<int> q1;
-        queue<int> q2;
-
-    void push(int x)
-    {
-
-        q2.push(x);
-
-        while (!q1.empty())
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
-        swap(q1, q2);
-    }
-
-    int pop()
-    {
-        if (q1.empty()) return -1;
-         int data = q1.front();
-         q1.pop();
+class MyStack {
+public:
+    queue<int> q;
+    
+    void push(int x) {
         
+        q.push(x);
+        
+        for(int i=0; i<q.size()-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        
+        int data = q.front();
+        q.pop();
         return data;
     }
-
-    int top()
-    {
-        return q1.front();
+    
+    int top() {
+        return q.front();
+        
     }
-
-    bool empty()
-    {
-       return q1.empty();
+    
+    bool empty() {
+        return q.empty();
     }
 };
 
 /**
- *Your MyStack object will be instantiated and called as such:
- *MyStack* obj = new MyStack();
- *obj->push(x);
- *int param_2 = obj->pop();
- *int param_3 = obj->top();
- *bool param_4 = obj->empty();
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
  */
