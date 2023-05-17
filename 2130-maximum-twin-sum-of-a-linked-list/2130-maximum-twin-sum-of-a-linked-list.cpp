1be29d0f7710bf722be7af281380cb22d3c77f82
Manish Kumar <84091739+manish079@users.noreply.github.com>
2023-05-17 22:18:53 +0530

    Time: 257 ms (78.39%), Space: 116.6 MB (75.42%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,45 +1,63 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *int val;
- *ListNode * next;
- *ListNode() : val(0), next(nullptr) {}
- *ListNode(int x) : val(x), next(nullptr) {}
- *ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        int pairSum(ListNode *head)
-        {
-
-            int max = INT_MIN;
-
-            vector<int> vec;
-
-            ListNode *ptr = head;
-
-           	//Pushing all nodes val into vector
-            while (ptr != NULL)
-            {
-                vec.push_back(ptr->val);
-                ptr = ptr->next;
-            }
-
-           	//traverse in vector
-
-            for (int i = 0; i <= vec.size() / 2 - 1; i++)
-            {
-
-                int twin = vec[i] + vec[vec.size() - 1 - i];
-
-                if (twin > max)
-                    max = twin;
-            }
-            
-                return max;
-
-        }
+class Solution {
+public:
     
+   
+    
+    int pairSum(ListNode* head) {
+        
+        // Approach-3 Using - reverse list after n/2
+        
+        int ans = 0;
+        
+        // 1. find mid
+         
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+        }
+        
+        ListNode *mid = slow;
+        
+        //2. reverse 2nd half of list
+       ListNode *prev = NULL;
+        ListNode *nextPtr = NULL;
+        
+        while(mid!=NULL){
+            
+            nextPtr  = mid->next;
+            mid->next = prev;
+            
+            prev = mid;
+            mid = nextPtr;
+        }
+        
+    
+        //3.Compute sum
+        
+        ListNode *curr = head;
+        while(prev!=NULL){
+            
+            ans = max(ans, curr->val + prev->val);
+            curr = curr->next;
+            prev = prev->next;
+        }
+        
+        return ans;
+               
+        
+    }
 };
